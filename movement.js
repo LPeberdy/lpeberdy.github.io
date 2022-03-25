8939336a66d77f7d8538f0233f11c7b82c77af39
lpeberdy <laurence.peberdy@gmail.com>
2022-03-25 15:40:35 +0000

    added touch move event listener

--- movement.js
+++ movement.js
@@ -14,3 +14,20 @@ document.addEventListener("mousemove", function (event) {
   
   target.style.transform = "rotateX(" + angleX + "deg) rotateY(" + angleY + "deg)"
 })
+
+document.addEventListener("touchmove", function (event) {
+  
+  const x = event.pageX
+  const y = event.pageY
+  
+  const target = document.querySelector(".card")
+  const targetCoords = target.getBoundingClientRect()
+  
+  const targetX = targetCoords.left + (target.offsetWidth / 2)
+  const targetY = targetCoords.top + (target.offsetHeight / 2)
+  
+  const angleX = (targetY - y) / 25
+  const angleY = (targetX - x) / - 25
+  
+  target.style.transform = "rotateX(" + angleX + "deg) rotateY(" + angleY + "deg)"
+})
